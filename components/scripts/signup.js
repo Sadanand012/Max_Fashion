cba58958bd1b43b7f55dfa51fcd7f46cb321db4e
Raj <officialraj480@gmail.com>
2022-08-27 21:59:56 +0530

    day 5

---
+++ components/scripts/signup.js
@@ -0,0 +1,25 @@
+document.getElementById("myForm").addEventListener("submit", myFun);
+	 data = JSON.parse(localStorage.getItem("Signup")) || [];
+	function myFun(event) {
+		event.preventDefault();
+		var obj = {
+			name: document.getElementById("fname").value,
+			lname: document.getElementById("lname").value,
+			email: document.getElementById("email").value,
+			uName: document.getElementById("userName").value,
+			password: document.getElementById("password").value,
+		};
+        if(document.getElementById("fname").value==""||document.getElementById("lname").value=="" || document.getElementById("email").value==""||document.getElementById("userName").value=="" ||document.getElementById("password").value=="")
+        {
+                alert("Fill all the fields")
+        }
+        else{
+            data.push(obj);
+        console.log(obj);
+		localStorage.setItem("Signup", JSON.stringify(data));
+		alert("Account Created");
+		window.location.href = "Login.html";
+        }
+		
+	}
+
