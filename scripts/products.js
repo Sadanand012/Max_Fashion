3120f5ccd053b35af35db8aeb60cf5ab4eb3af38
Madhukar <kakadem.b.99@gmail.com>
2022-08-28 01:20:16 +0530

    dissable div designed & data collected for men tshirt/causual dress ,slider built

--- scripts/products.js
+++ scripts/products.js
@@ -6,7 +6,7 @@ let Favourites = JSON.parse(localStorage.getItem("FavouriteProducts")) || [];
 
 async function getData() {
   let search = "Soft Toys";
-  let query = "womenKurta";
+  let query = "menCaps";
   searchTitle.innerText = search;
 
   try {
@@ -64,7 +64,7 @@ let Display = (data) => {
 
     // EventListener for add Favourite product
     addToFavorate.addEventListener("click", function () {
-      // addFavourites(this, data, i);
+      addFavourites(this, data, i);
     });
 
     let div1 = createDiv("slider");
@@ -133,9 +133,252 @@ let Display = (data) => {
     );
     Favourites.forEach((el) => {
       if (el.title == data[i].title) {
-        // addFavourites(addToFavorate);
+        addFavourites(addToFavorate);
       }
     });
-    container.append(div);
+    let div2 = createDiv("disableBox");
+    let productDetail = createDiv("productDetail");
+    let productColor = createDiv("productColor");
+
+    let colorBtn = createDiv("colorBtn");
+
+    let colorImg = create("img");
+    colorImg.src = images[0];
+    let colorSpan = create("span", color[0]);
+    let colorDownArray = create("img");
+    colorBtn.append(colorImg, colorSpan, colorDownArray);
+    let colorDiv = createDiv("colorDiv");
+
+    console.log(color.length);
+    let elementImage = image;
+    if (color.length > 1) {
+      colorDownArray.src = "./images/icons8-down-24.png";
+      color.forEach((el) => {
+        let insideColorDiv = createDiv("insideColorDiv");
+
+        let colorImg = create("img");
+
+        data.forEach((element) => {
+          if (element.title == title && element.image.includes(el)) {
+            console.log(element, "****");
+            colorImg.src = element.image;
+            elementImage = element.image;
+          }
+        });
+
+        let colorSpan = create("span", el);
+
+        insideColorDiv.append(colorImg, colorSpan);
+
+        // eventlitsner for change color
+        insideColorDiv.addEventListener("click", function () {
+          changeImageColor(colorBtn, elementImage, this, div1, colorDownArray);
+          console.log(this);
+          colorDiv.style.display = "none";
+        });
+        colorDiv.append(insideColorDiv);
+      });
+    }
+
+    productColor.append(colorBtn, colorDiv);
+
+    // calling addevent lisner for enabling colorDiv
+    colorBtn.addEventListener("click", function () {
+      openDiv(colorDownArray, colorDiv, this);
+      sizeDiv.style.display = "none";
+    });
+
+    // size Div
+    let ProductSize = createDiv("ProductSize");
+
+    let sizeBtnDiv = createDiv("sizeBtnDiv");
+
+    let sizeBtn = create("span");
+    let downArrow = create("img");
+
+    sizeBtnDiv.append(sizeBtn, downArrow);
+
+    // calling addevent lisner for enabling sizeDiv
+    sizeBtnDiv.addEventListener("click", function () {
+      openDiv(downArrow, sizeDiv, this);
+      colorDiv.style.display = "none";
+    });
+    let sizeDiv = createDiv("sizeDiv");
+
+    if (size.length > 0) {
+      sizeBtn.innerText = "Select Size";
+      downArrow.src = "./images/icons8-down-24.png";
+      size.forEach((ele) => {
+        let size = create("p", ele);
+        // select size call function
+        size.addEventListener("click", function () {
+          selectSize(sizeBtn, this, downArrow);
+          console.log(this);
+          sizeDiv.style.display = "none";
+        });
+
+        sizeDiv.append(size);
+      });
+    }
+    ProductSize.append(sizeBtnDiv, sizeDiv);
+
+    // disble size btn when data not available
+    if (size.length == 0) {
+      productDetail.append(productColor);
+      productColor.style.width = "100%";
+    } else {
+      productDetail.append(productColor, ProductSize);
+    }
+
+    // basket div
+    let addBasketBtn = create("div", "ADD TO BASKET");
+    addBasketBtn.className = "addBasketBtn";
+
+    // add product to cart
+    addBasketBtn.addEventListener("click", function () {
+      if (sizeBtnDiv.innerText !== "Select Size") {
+        maxCart.push(data[i]);
+        localStorage.setItem("maxCart", JSON.stringify(maxCart));
+        alert("product added Succesfully!");
+      } else {
+        alert("please select product size");
+      }
+    });
+
+    div2.append(productDetail, addBasketBtn);
+
+    productDiv.append(div, div2);
+    productDiv.addEventListener("mouseenter", function () {
+      // if (call) {
+      this.style.boxShadow = `rgba(0, 0, 0, 0.25) 0px 54px 55px,
+          rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+          rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px`;
+      this.style.padding = "20px";
+      this.style.backgroundColor = "white";
+
+      enable(div2, leftArrow, rightArrow, this);
+    });
+    productDiv.addEventListener("mouseleave", function () {
+      this.style.boxShadow = "";
+      this.style.padding = "0px";
+      this.style.backgroundColor = "";
+      enable(div2, leftArrow, rightArrow, this);
+      disable(div2, colorDiv, sizeDiv, leftArrow, rightArrow);
+      // console.log(event);
+    });
+
+    container.append(productDiv);
   });
 };
+
+let id;
+
+function sliderStop(image, reset) {
+  clearInterval(id);
+  reset.innerHTML = null;
+
+  let img = create("img");
+  img.src = image;
+  reset.append(img);
+  reset.style.transform = "translateX(0%)";
+  reset.style.transition = "0s";
+  console.log("***", id, img, reset);
+}
+
+// function sliderData(images, div1) {
+//   div1.addEventListener("mouseenter", function () {
+//     sliderData(images, div1);
+//   });
+// }
+
+let sliderStart = (images, div1) => {
+  let i = 1;
+  let z = 0;
+  id = setInterval(function () {
+    // div1.innerHTML = null;
+
+    if (i === images.length) {
+      i = 0;
+      // clearInterval(id);
+    }
+
+    let img = create("img");
+    img.src = images[i];
+    div1.append(img);
+    console.log(i, images.length);
+    div1.style.transition = "0.7s";
+    div1.style.transform = `translateX(${z * -100}%)`;
+
+    i++;
+    z++;
+  }, 2000);
+};
+
+function openDiv(Arrow, enableDiv, parent) {
+  console.log(enableDiv.innerHTML == "");
+  if (enableDiv.style.display == "block") {
+    Arrow.src = "./images/icons8-down-24.png";
+    enableDiv.style.display = "none";
+    parent.style.border = "0.3px solid rgb(173, 173, 236)";
+  } else {
+    if (enableDiv.innerHTML !== "") {
+      enableDiv.style.display = "block";
+      Arrow.src = "./images/icons8-up-24.png";
+    }
+    parent.style.border = " 1px solid blue";
+  }
+}
+
+function changeImageColor(colorBtn, Image, ImageDiv, container) {
+  colorBtn.innerHTML = ImageDiv.innerHTML;
+  let img = create("img");
+  console.log(Image, "***");
+  container.innerHTML = null;
+
+  img.src = Image;
+  container.append(img);
+  let Arrow = create("img");
+  Arrow.src = "./images/icons8-down-24.png";
+  colorBtn.append(Arrow);
+  // call = false;
+  // sliderData(Images, container);
+}
+function selectSize(sizeBtn, size, Arrow) {
+  sizeBtn.innerHTML = size.innerHTML;
+  sizeBtn.style.color = "black";
+  Arrow.src = "./images/icons8-down-24.png";
+}
+
+// add Favourites products
+function addFavourites(favor, data, product = null) {
+  if (favor.style.backgroundColor == "pink") {
+    favor.style.backgroundColor = "white";
+    console.log(Favourites);
+    Favourites = Favourites.filter((el) => {
+      return el.id !== data[product].id;
+    });
+  } else {
+    favor.style.backgroundColor = "pink";
+    if (product) Favourites.push(data[product]);
+  }
+  console.log(Favourites);
+  localStorage.setItem("FavouriteProducts", JSON.stringify(Favourites));
+}
+
+function enable(...Display) {
+  Display.forEach((el) => {
+    el.style.display = "block";
+  });
+}
+function disable(...Display) {
+  Display.forEach((el) => {
+    if (el != "leftArrow" || el != "rightArrow") {
+      el.style.display = "none";
+    } else {
+      el.style.display = "block";
+    }
+  });
+  // disablDiv.style.display = "none";
+  // colorDiv.style.display = "none";
+  // sizeDiv.style.dis
+}
