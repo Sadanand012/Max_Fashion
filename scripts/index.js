21d16996660a531d55fbf3b69de9053dc2b1c137
Sadanand012 <sadamare8408@gmail.com>
2022-08-27 20:22:06 +0530

    homePage- women adding

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,7 @@ document.getElementById("offer_s").innerHTML = offer_navbar()
 document.getElementById("navbar").innerHTML = navbar()
 document.getElementById("footer").innerHTML = footer_home()
 
+let click_onwomen = document.querySelector(".category_btn5_1");
+click_onwomen.addEventListener("click",function(){
+    window.location.href = "homePage.html";
+})
